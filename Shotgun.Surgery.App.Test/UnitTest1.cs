bbe2124032c94449e5e4502a4686eedc61c9aca9
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-07-13 16:48:01 +0200

    chore: init project.

---
+++ Shotgun.Surgery.App.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Shotgun.Surgery.App.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
